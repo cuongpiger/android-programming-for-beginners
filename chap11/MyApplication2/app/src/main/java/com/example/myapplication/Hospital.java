eb8cab0b7e66acb5621653d2127ab5b876fd8974
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-12-01 21:18:17 +0700

    [chore] done chaper 11

---
+++ chap11/MyApplication2/app/src/main/java/com/example/myapplication/Hospital.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+
+import android.util.Log;
+
+
+class Hospital{
+    protected void healSoldier(Soldier soldierToHeal){
+        Log.i("Just arrived at healSoldier method = ", "" + soldierToHeal.getHealth());
+        int health = soldierToHeal.getHealth();
+        health = health + 10;
+        soldierToHeal.setHealth(health);
+        Log.i("Just finnishing heal soldier method = ", "" + soldierToHeal.getHealth());
+    }
+}
---
+++ chap11/MyApplication2/app/src/main/java/com/example/myapplication/Soldier.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+public class Soldier{
+    private int health;
+    public int getHealth(){
+        return health;
+    }
+
+    public void setHealth(int newHealth){
+        health = newHealth;
+    }
+}
+
