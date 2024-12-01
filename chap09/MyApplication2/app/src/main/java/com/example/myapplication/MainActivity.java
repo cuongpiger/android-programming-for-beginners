16a97488b44bb44a351ca375bf60b1ae1e684aed
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-12-01 20:09:56 +0700

    [chore] done chaper 09

---
+++ chap09/MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        String joinedString = joinThese("Methods ", "are ", "cool ");
+        Log.i("joinedString = ","" + joinedString);
+
+        float area  = getAreaCircle(5f);
+        Log.i("area = ","" + area);
+
+        int a = 0;
+        changeA(a);
+        Log.i("a = ","" + a);
+
+    }
+
+    String  joinThese(String a, String b, String c){
+        return a + b + c;
+    }
+
+    float getAreaCircle(float radius){
+
+        return 3.141f * radius * radius;
+
+    }
+
+    void changeA(int a){
+        a++;
+
+    }
+}
